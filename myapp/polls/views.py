e619661ef112639ee21e2de78611839670b6242f
Shyam Srinivasan <shyamsweth@gmail.com>
2023-07-04 09:03:58 +0530

    new views and linked urls

--- myapp/polls/views.py
+++ myapp/polls/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World! Polls Index")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse('You are looking at question %s' % question_id)
+
+
+def results(request, question_id):
+    response = 'You are looking at results of question %s'
+    return HttpResponse(response % question_id)
